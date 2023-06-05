174c1073a39f4af0a4a21402a03fb552b464dbe4
Ruben Hernandez <rubendez21@gmail.com>
2023-06-05 14:00:03 -0600

    Actualización de check_key

--- check_key.c
+++ check_key.c
@@ -6,38 +6,36 @@
 #include <sys/ioctl.h>
 
 char check_key(void);
-
 char check_key(void){
 
-    char c;
+    char c; //Caracter que se lee si el buffer no está vacío.
 
+    //Se establece en modo crudo la terminal.
     struct termios term;
     tcgetattr(0, &term);
-
-    //struct termios term2 = term;
     term.c_lflag &= ~(ICANON | ECHO);
     tcsetattr(0, TCSANOW, &term);
 
+    //Número de bytes pendientes en el buffer stdin.
     int bytesWaiting;
 
-    //for(;;){ 
+        /*Lee la cantidad de datos en el buffer. Si es mayor a 0 lee un caracter
+        y después limpia el buffer de entrada*/
         ioctl(0, FIONREAD, &bytesWaiting);
         if(bytesWaiting > 0){
             c = getc(stdin);
-            //printf("DATA %c \n", c);
             tcflush(0, TCIFLUSH);
-            //break;
             }
         else
             c = 0;
-            //printf("NO DATA\n");
 
-        sleep(0.9);
-    //}
+        sleep(0.9); //Delay (podría quitarse).
 
+    /*Termina la función de checar el teclado y regresa la consola
+    a modo cooked*/
     term.c_lflag |= ICANON | ECHO;
     tcsetattr(0, TCSANOW, &term);
 
+    //Si se leyó un caracter se retorna este sino se retorna 0.
     return c;
-
 }
